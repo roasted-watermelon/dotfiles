bceda3d995c1221f62610b0db3c55b24c7c4af8f
NightCore <nightcore@endeavour>
2024-05-24 20:13:37 +0530

    use write_file to make fixflameshot

--- system/system_settings.sh
+++ system/system_settings.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+source <(cat ../.common/*)
+
 # enable bluetooth
 sudo systemctl enable bluetooth
 sudo systemctl start bluetooth
+
+script_dir="/usr/local/bin"
+
+create_script() {
+  local file_name="$1"
+  local contents="$2"
+  local tmp_file="$(write_file "$contents")"
+  chmod +x "$tmp_file"
+  sudo mv "$tmp_file" "${script_dir}/${file_name}"
+}
+
+if [[ -n "$(echo $WAYLAND_DISPLAY)" ]]; then
+  create_script "fixflameshot" "env QT_QPA_PLATFORM=wayland flameshot \"\$@\""
+else
+  create_script "fixflameshot" "flameshot \"\$@\""
+fi
