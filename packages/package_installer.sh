9aca1c81843431408faebf32828554284f0ce670
NightCore <nightcore@endeavour>
2024-05-25 10:11:25 +0530

    package_installer modified to install os specific apps

--- packages/package_installer.sh
+++ packages/package_installer.sh
@@ -7,19 +7,32 @@ input_packages="$@"
 ID=`awk -F= '$1=="ID" { print $2 ;}' /etc/os-release | sed -e 's/^"//' -e 's/"$//'`
 ID_LIKE=`awk -F= '$1=="ID_LIKE" { print $2 ;}' /etc/os-release | sed -e 's/^"//' -e 's/"$//'`
 
+install() {
+  echo "Packages: $packages"
+  install_packages $packages
+}
+
+if [[ -n "$input_packages" ]]; then
+  packages="$input_packages"
+  install
+  exit
+fi
+
+packages=$(remove_comments packages.list | one_liner)
+
+os=""
+
 if [[ "${ID_LIKE,,}" == "arch" || "${ID,,}" == "arch" ]]; then
   echo "Arch linux detected"
+  os="arch"
   source .installer.arch
 else
   echo "!!! OS NOT DETECTED in package installer !!!"
   exit
 fi
 
-if [[ -z "$input_packages" ]]; then
-  packages=$(remove_comments packages.list | one_liner)
-else
-  packages="$input_packages"
+if [[ -n "$os" && -f "packages.${os}.list" ]]; then
+  packages="$packages $(remove_comments packages.${os}.list | one_liner)"
 fi
 
-echo "Packages: $packages"
-install_packages $packages
+install
