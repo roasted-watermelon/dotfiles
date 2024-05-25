e1612a538d0b22f07e290eb7f6350a840fc8334e
NightCore <nightcore@endeavour>
2024-05-25 21:18:31 +0530

    define get_os_type and use it in package_installer.sh

--- packages/package_installer.sh
+++ packages/package_installer.sh
@@ -4,13 +4,20 @@ source <(cat ../.common/*)
 
 input_packages="$@"
 
-ID=`awk -F= '$1=="ID" { print $2 ;}' /etc/os-release | sed -e 's/^"//' -e 's/"$//'`
-ID_LIKE=`awk -F= '$1=="ID_LIKE" { print $2 ;}' /etc/os-release | sed -e 's/^"//' -e 's/"$//'`
-
 install() {
   echo "Packages: $packages"
   install_packages $packages
 }
+
+os="$(get_os_type)"
+
+if [[ -z "$os" ]]; then
+  echo "!!! OS NOT DETECTED in package installer !!!"
+  exit
+fi
+
+echo "Detected OS type: $os"
+source ".installer.${os}"
 
 if [[ -n "$input_packages" ]]; then
   packages="$input_packages"
@@ -20,18 +27,7 @@ fi
 
 packages=$(remove_comments packages.list | one_liner)
 
-os=""
-
-if [[ "${ID_LIKE,,}" == "arch" || "${ID,,}" == "arch" ]]; then
-  echo "Arch linux detected"
-  os="arch"
-  source .installer.arch
-else
-  echo "!!! OS NOT DETECTED in package installer !!!"
-  exit
-fi
-
-if [[ -n "$os" && -f "packages.${os}.list" ]]; then
+if [[ -f "packages.${os}.list" ]]; then
   packages="$packages $(remove_comments packages.${os}.list | one_liner)"
 fi
 
