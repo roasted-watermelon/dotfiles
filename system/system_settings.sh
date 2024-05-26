819bf352b4d764188ee7d3349365363a37b601b3
NightCore <nightcore@endeavour>
2024-05-26 17:05:44 +0530

    firefox-profile-switcher-connector

--- system/system_settings.sh
+++ system/system_settings.sh
@@ -16,8 +16,17 @@ create_script() {
   sudo mv "$tmp_file" "${script_dir}/${file_name}"
 }
 
+create_user_file() {
+  local file_path="$1"
+  local contents="$2"
+  local tmp_file="$(write_file "$contents")"
+  eval mv "$tmp_file" "$file_path" # eval is used to process ~ (home)
+}
+
 if [[ -n "$(echo $WAYLAND_DISPLAY)" ]]; then
   create_script "fixflameshot" "env QT_QPA_PLATFORM=wayland flameshot \"\$@\""
 else
   create_script "fixflameshot" "flameshot \"\$@\""
 fi
+
+create_user_file "~/.config/firefoxprofileswitcher/config.json" "{\"browser_binary\": \"/usr/bin/firefox\"}"
