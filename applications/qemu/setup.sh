36fe85f7817acb7790caf93cf175133004403bda
NightCore <nightcore@endeavour>
2024-05-27 19:28:34 +0530

    add comments on qemu

--- applications/qemu/setup.sh
+++ applications/qemu/setup.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+# WINDOWS Stuff
+#
+# 1. HOST - Set "Video" to "Virtio".
+# 2. GUEST - Install VirtIO drivers after mounting ISO:
+# https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/
+# 3. GUEST - Install spice guest tools:
+# https://www.spice-space.org/download.html#windows-binaries
+# 4. HOST - Enable 3D Acceleration:
+# https://discourse.nixos.org/t/opengl-can-not-be-used-with-libvirt/43688/6
+# 5. Share a folder:
+# https://www.debugpoint.com/kvm-share-folder-windows-guest/
+# Virtiofsd (may be optional):
+# https://discourse.nixos.org/t/virt-manager-cannot-find-virtiofsd/26752
+# 6. Enable clipboard sharing (optional):
+# https://bbs.archlinux.org/viewtopic.php?pid=2167226#p2167226
+
+# More on share directory:
+# https://www.debugpoint.com/share-folder-virt-manager/
+
 cd ..
 ./.common_setup.sh "qemu"
 
