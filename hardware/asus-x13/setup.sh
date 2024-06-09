2b1f21c44b736fc69d90d692df6bc736c4f3ed01
NightCore <nightcore@endeavour>
2024-06-10 04:06:11 +0530

    quiet option for fan-speed-toggle

--- hardware/asus-x13/setup.sh
+++ hardware/asus-x13/setup.sh
@@ -10,12 +10,15 @@ create_script "fan-speed-toggle" "
 
 current_profile=\$(asusctl profile -p | awk '{print \$NF}')
 
+mode_to_set=\"\$1\"
+is_quiet=\"\$2\"
+
 fans_max() {
   asusctl fan-curve -m \$current_profile -f cpu -D 30c:100%,49c:100%,59c:100%,69c:100%,79c:100%,89c:100%,99c:100%,109c:100%
   asusctl fan-curve -m \$current_profile -f gpu -D 30c:100%,49c:100%,59c:100%,69c:100%,79c:100%,89c:100%,99c:100%,109c:100%
   asusctl fan-curve -e true -m \$current_profile
   echo \"Fans MAX speed!\"
-  notify-send -a \"Fan toggle\" \"Fans MAX speed!\"
+  [[ -z \$is_quiet ]] && notify-send -a \"Fan toggle\" \"Fans MAX speed!\"
 }
 
 fans_zero() {
@@ -23,16 +26,14 @@ fans_zero() {
   asusctl fan-curve -m \$current_profile -f gpu -D 30c:0%,49c:0%,59c:0%,69c:0%,79c:0%,89c:0%,99c:0%,109c:0%
   asusctl fan-curve -e true -m \$current_profile
   echo \"Fans zero speed!\"
-  notify-send -a \"Fan toggle\" -u critical \"Fans zero speed!!!!\"
+  [[ -z \$is_quiet ]] && notify-send -a \"Fan toggle\" -u critical \"Fans zero speed!!!!\"
 }
 
 fans_default() {
   asusctl fan-curve -d
   echo \"Fans default speed.\"
-  notify-send -a \"Fan toggle\" \"Fans default speed\"
+  [[ -z \$is_quiet ]] && notify-send -a \"Fan toggle\" \"Fans default speed\"
 }
-
-mode_to_set=\"\$1\"
 
 max_beginning=\`asusctl fan-curve -g | head -n 1 | awk -F',' '{print \$2}' | grep \":100%\"\`
 zero_ending=\`asusctl fan-curve -g | head -n 1 | awk -F',' '{print \$NF}' | grep \":0%\"\`
