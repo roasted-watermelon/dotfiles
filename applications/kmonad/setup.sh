71ba6506b4b709cb1bc2b99400687ec08828f32e
NightCore <nightcore@endeavour>
2024-09-08 05:30:56 +0530

    kmonad use binary from github

--- applications/kmonad/setup.sh
+++ applications/kmonad/setup.sh
@@ -2,15 +2,17 @@
 
 source <(cat ../../.common/*)
 
-common_setup "../../packages" "applications/kmonad"
-
-cd "../applications/kmonad"
+download_url=`curl -s https://api.github.com/repos/kmonad/kmonad/releases/latest | grep browser_download_url | awk '{print $2}' | sed 's/"//g'`
+wget $download_url -O /tmp/kmonad
+sudo mv /tmp/kmonad /usr/bin/kmonad
 
 sudo mkdir -p /etc/kmonad
 sudo cp asus-x13.kbd /etc/kmonad/config.kbd
 sudo cp kmonad.service /etc/kmonad/kmonad.service
 
-sudo ln -s /etc/kmonad/kmonad.service /etc/systemd/system/kmonad.service
+service="/usr/lib/systemd/system/kmonad.service"
+sudo rm "$service"
+sudo ln -s /etc/kmonad/kmonad.service "$service"
 
 sudo systemctl enable kmonad.service
 sudo systemctl start kmonad.service
