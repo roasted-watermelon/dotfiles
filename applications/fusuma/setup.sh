847a39cc126a1b70c9561aa6c5e74f7e68db1213
NightCore <nightcore@endeavour>
2024-09-11 06:48:41 +0530

    fusuma - fix gems not being installed

--- applications/fusuma/setup.sh
+++ applications/fusuma/setup.sh
@@ -6,11 +6,11 @@ common_setup "../../packages" "applications/fusuma"
 
 cd "../applications/fusuma"
 
-sudo gpasswd -a $USER input
-newgrp input
-
 mkdir -p ~/.config/fusuma
 cp config.yml ~/.config/fusuma/
 
 sudo gem install fusuma --no-user-install
 sudo gem install fusuma-plugin-sendkey --no-user-install
+
+sudo gpasswd -a $USER input
+newgrp input
